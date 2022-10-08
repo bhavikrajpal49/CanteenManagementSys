60bd4da8287b18ec1076f4a1f643ba833c8abd58
Bhavik Rajpal <bhavik.sk.rajpal@gmail.com>
2022-10-08 17:08:16 +0530

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,20 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZBWseT7cgz_H2Ajg5YyJxW6QzI-FDws4",
+    authDomain: "canteenprj.firebaseapp.com",
+    projectId: "canteenprj",
+    storageBucket: "canteenprj.appspot.com",
+    messagingSenderId: "338788462140",
+    appId: "1:338788462140:web:14eafb95195dc2c8bddd39",
+    measurementId: "G-QXFMYXP44Q"
+};
+
+const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
+
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, firestore, storage };
---
+++ src/utils/data.js
@@ -0,0 +1,78 @@
+//import I1 from "../img/i1.png";
+//import F1 from "../img/f1.png";
+//import C3 from "../img/c3.png";
+//import Fi1 from "../img/fi1.png";
+import Fast from "../img/fastfood.png"
+import Coke from "../img/coke.png"
+import thali from "../img/splthali.png"
+import ice from "../img/starwicemain.png"
+
+export const heroData = [
+  {
+    id: 1,
+    name: "Icecream",
+    decp: "Strawberry",
+    price: "25",
+    imageSrc: ice,
+  },
+  {
+    id: 2,
+    name: "Fast-food",
+    decp: "Samosa",
+    price: "15",
+    imageSrc: Fast,
+  },
+  {
+    id: 3,
+    name: "Mini-Lunch",
+    decp: "Thali",
+    price: "45",
+    imageSrc: thali,
+  },
+  {
+    id: 4,
+    name: "Pepsi",
+    decp: "Cold pepsi",
+    price: "20",
+    imageSrc: Coke,
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    name: "Lunch",
+    urlParamName: "lunch",
+  },
+  {
+    id: 2,
+    name: "Fast Food",
+    urlParamName: "fast food",
+  },
+  {
+    id: 3,
+    name: "South Indian",
+    urlParamName: "south indian",
+  },
+  {
+    id: 4,
+    name: "Milksahakes",
+    urlParamName: "milkshakes",
+  },
+  {
+    id: 5,
+    name: "Chinese",
+    urlParamName: "chinese",
+  },
+  {
+    id: 6,
+    name: "Icecreams",
+    urlParamName: "icecreams",
+  },
+
+  {
+    id: 7,
+    name: "Soft Drinks",
+    urlParamName: "drinks",
+  },
+];
